2c487ace78d5e8c8ab5367348170fbbb64577335
SVSS13 <sujal.surampudi13@gmail.com>
2026-05-14 23:37:12 +0530

    FINAL DEPLOYMENT BACKEND READY

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -50,6 +50,14 @@ ALLOWED_HOSTS = [
 
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+
+    "https://*.onrender.com",
+
+    "https://*.vercel.app",
+
+]
+
 
 # Application definition
 
@@ -145,7 +153,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
